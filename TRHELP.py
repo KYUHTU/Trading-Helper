ba2335c96fac761ac80a940c2decc060a979311e
Keezyslimb <74580050+KYUHTU@users.noreply.github.com>
2024-09-25 16:48:57 +0700

    Add files via upload

--- TRHELP.py
+++ TRHELP.py
@@ -10,20 +10,20 @@ eth = TA_Handler(
 while True:
     if (eth.get_analysis().indicators['RSI'] > 50):
         print(round((eth.get_analysis().indicators['RSI'] - 50) * 5), "% sell", end="\r")
-        if (round((eth.get_analysis().indicators['RSI'] - 50) * 5) >= 75):
-            print("strong sell", end="\t")
-        elif (round((eth.get_analysis().indicators['RSI'] - 50) * 5) >= 50):
-            print("sell", end="\t")
-        elif (round((eth.get_analysis().indicators['RSI'] - 50) * 5) < 50):
-            print("weak sell", end="\t")
+        #if (round((eth.get_analysis().indicators['RSI'] - 50) * 5) >= 75):
+            #print("strong sell", end="\t")
+        #elif (round((eth.get_analysis().indicators['RSI'] - 50) * 5) >= 50):
+            #print("sell", end="\t")
+        #elif (round((eth.get_analysis().indicators['RSI'] - 50) * 5) < 50):
+            #print("weak sell", end="\t")
     elif (eth.get_analysis().indicators['RSI'] < 50):
         print(round((50 - eth.get_analysis().indicators['RSI']) * 5), "% buy", end="\r")
-        if (round((50 - eth.get_analysis().indicators['RSI']) * 5) >= 75):
-            print("strong buy", end="\t")
-        elif (round((50 - eth.get_analysis().indicators['RSI']) * 5) >= 50):
-            print("buy", end="\t")
-        elif (round((50 - eth.get_analysis().indicators['RSI']) * 5) < 50):
-            print("buy", end="\t")
+        #if (round((50 - eth.get_analysis().indicators['RSI']) * 5) >= 75):
+            #print("strong buy", end="\t")
+        #elif (round((50 - eth.get_analysis().indicators['RSI']) * 5) >= 50):
+            #print("buy", end="\t")
+        #elif (round((50 - eth.get_analysis().indicators['RSI']) * 5) < 50):
+            #print("buy", end="\t")
             
     
         
